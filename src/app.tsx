36e0c7373212cd0e082518ac34df11537c3cecf5
郑壮 <zhengzhuang@zhengzhngdeMBP2.www.idazoo.com>
2022-11-04 10:06:23 +0800

    feat: 优化项目结构

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: {zhengzhuang}
+ * @Date: 2022-08-16 17:58:57
+ * @LastEditors: {zhengzhuang}
+ * @LastEditTime: 2022-11-04 10:06:15
+ * @Description:
+ */
 import React, { FC } from 'react'
 import { Provider } from 'react-redux'
 import configStore from '@/store'
